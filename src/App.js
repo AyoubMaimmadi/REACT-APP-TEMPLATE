6a2117b0dea7f5d42aceea8d43ddf22faeb023de
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-20 18:49:35 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  const Add = () => {
-    setCount((x) => x + 1)
-  }
-  const Sub = () => {
-    setCount((x) => x - 1)
-  }
-  const Reset = () => {
-    setCount((x) => x * 0)
-  }
-
-  return (
-    <>
-      <button onClick={Add}>+</button>
-      <span>&nbsp;{count}&nbsp;</span>
-      <button onClick={Sub}>-</button>
-      <br />
-      <button onClick={Reset}>reset</button>
-    </>
-  )
+const App = () => {
+  return <div>App</div>
 }
 
 export default App
